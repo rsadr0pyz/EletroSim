470ec593036f1e03bb81d6384f0686ec9441ad78
rsadr0pyz <rsadr0pyz@gmail.com>
2025-08-19 08:40:27 -0300

    Trying yee grid

--- testes/field.py
+++ testes/field.py
@@ -2,41 +2,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import matplotlib
+import matplotlib.animation as ani
+import time
+from enum import Enum
+
+class GridSet(Enum):
+    BASE = 0
+    SUPPORT = 1
 
 class DiscreteVectorField:
     
-    def __init__(self, side_length: int):
+    
+    def __init__(self, side_length: int, grid_set : GridSet):
         self.side_length = side_length
 
-        # X, Y, Z (side_length) positions + 3 components (x,y,z) for each vector
-        self.Fx = np.zeros([side_length, side_length, side_length]) #np.random.randint(low=-5, high=5, size=[side_length]*3)
-        self.Fy = np.zeros([side_length, side_length, side_length]) #np.random.randint(low=-5, high=5, size=[side_length]*3)
-        self.Fz = np.zeros([side_length, side_length, side_length]) #np.random.randint(low=-5, high=5, size=[side_length]*3)
-        self.curl = np.zeros([side_length-1, side_length-1, side_length-1, 3])
+        if grid_set == GridSet.BASE:
+            fx = np.zeros([side_length, side_length, side_length]) 
+            fy = np.zeros([side_length, side_length, side_length]) 
+            fz = np.zeros([side_length, side_length, side_length]) 
+        elif grid_set == GridSet.SUPPORT:
+            fx = np.zeros([side_length-1, side_length-1, side_length]) 
+            fy = np.zeros([side_length-1, side_length-1, side_length]) 
+            fz = np.zeros([side_length, side_length, side_length]) 
+        self.field = np.stack((fx, fy, fz), axis=-1)
 
     def compute_curl(self):
-        self.pypx = np.gradient(self.Fy, axis=0)
-        self.pzpx = np.gradient(self.Fz, axis=0)
+        fx = self.field[:,:,:,0]
+        fy = self.field[:,:,:,1]
+        fz = self.field[:,:,:,2]
 
-        self.pxpy = np.gradient(self.Fx, axis=1)
-        self.pzpy = np.gradient(self.Fz, axis=1)
+        self.pypx = np.diff(fy, axis=0)[:, 0:-1,:]
+        self.pzpx = np.diff(fz, axis=0)[:, :, 0:-1]
 
-        self.pxpz = np.gradient(self.Fx, axis=2)
-        self.pypz = np.gradient(self.Fy, axis=2)
+        self.pxpy = np.diff(fx, axis=1)[0:-1, :, :]
+        self.pzpy = np.diff(fz, axis=1)[:, :, 0:-1]
+
+        self.pxpz = np.diff(fx, axis=2)[0:-1, :, :]
+        self.pypz = np.diff(fy, axis=2)[:, 0:-1, :]
         
         x_comp = self.pzpy - self.pypz
         y_comp = self.pxpz - self.pzpx
         z_comp = self.pypx - self.pxpy
         
-        np.stack((x_comp, y_comp, z_comp), axis=-1, out=self.curl)
-
-    def curl_at(self, pos):
-        x_comp = self.pzpy[*pos] - self.pypz[*pos]
-        y_comp = self.pxpz[*pos] - self.pzpx[*pos]
-        z_comp = self.pypx[*pos] - self.pxpy[*pos]
-
-        return np.array([x_comp, y_comp, z_comp])
-
+        return x_comp, y_comp, z_comp
 
 # CONSTANTS
 
@@ -54,25 +62,25 @@ SIMULATION_TIME = 10
 TIME_SCALE = 1e-6
 
 # DERIVED PARAMETERS
-frame_time = 1/FPS
+frame_time = 1/FPS 
 ds = c0 / (MAXIMUM_FREQUENCE * 10)
 dt = ds/(2 * c0)
 jmax = int(SPACE_SIDE_LENGTH  / ds)
 
-h_field = DiscreteVectorField(jmax)
-e_field = DiscreteVectorField(jmax-1)
+e_field = DiscreteVectorField(jmax)
+h_field = DiscreteVectorField(jmax-1)
 
 # PLOTS
-
+jsource = int(jmax/2)
 fig = plt.figure()
 ax = fig.add_subplot()
-Explot = ax.pcolormesh(ez)
-Explot.set_norm(matplotlib.colors.Normalize(vmin=-0.1, vmax=0.1))
+Explot = ax.pcolormesh(e_field.field[:, jsource, :, 0])
+Explot.set_norm(matplotlib.colors.Normalize(vmin=-3, vmax=3))
 
 
-# SOURCE
+## SOURCE
 
-jsource = int(jmax/2)
+
 def source_function(t):
     f0 = 3e6
     w0 = 2 * np.pi * f0
@@ -82,21 +90,27 @@ def source_function(t):
 # SIMULATION
 
 simulation_steps_per_frame = int((frame_time * TIME_SCALE) / dt)
-max_frames = int(SIMULATION_TIME / frame_time)
-print((frame_time * TIME_SCALE) / dt)
+max_frames = int(SIMULATION_TIME / (simulation_steps_per_frame * dt))
 
 current_n = 0
 def update(_):
-    global h_field, ez, current_n
+    global h_field, e_field, current_n
     for n in range(current_n, current_n+simulation_steps_per_frame):
-        h_field.compute_curl()
-        
-        ez[0:jmax-1, 0:jmax-1] = ez[0:jmax-1, 0:jmax-1] - 0.5 * h_field.curl
-        ez[jsource, jsource] += source_function(n*dt)
+        hrx, hry, hrz = h_field.compute_curl()
+        e_field.field[0:-1, 1:-1, 1:-1, 0] += 0.5 * hrx
+        e_field.field[1:-1, 0:-1, 1:-1, 1] += 0.5 * hry
+        e_field.field[1:-1, 1:-1, 0:-1, 2] += 0.5 * hrz
+
+        e_field.field[jsource, jsource, jsource, 2] = source_function(n*dt)
+
+        erx, ery, erz = e_field.compute_curl()
+        h_field.field[:, :, :, 0] -= 0.5 * erx[0:-1, :, :]
+        h_field.field[:, :, :, 1] -= 0.5 * ery[:, 0:-1, :]
+        h_field.field[:, :, :, 2] -= 0.5 * erz[:, :, 0:-1]
 
     current_n += simulation_steps_per_frame
-    Explot.set_array(ez_field)
+    Explot.set_array(e_field.field[:, :, jsource, 2])
     ax.set_title(int(current_n * dt / TIME_SCALE))
 
-animation = ani.FuncAnimation(fig=fig, func=update, frames=SIMULATION_TIME * FPS, interval=1000/FPS)
+animation = ani.FuncAnimation(fig=fig, func=update, frames=max_frames, interval=1000/FPS)
 plt.show()
